2a071a4a42924505e3db1dfb5746f98f5930b276
Dilsahd Alam <rayeendilshadn@gmail.com>
2024-03-03 12:42:32 +0530

    api key

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc_demo/feature/weather/bloc/weather_bloc.dart';
 import 'package:flutter_bloc_demo/feature/weather/repository/repository.dart';
 import 'package:flutter_bloc_demo/feature/weather/repository/repository_provider.dart';
 import 'package:flutter_bloc_demo/feature/weather/screens/home_screen.dart';
+import 'package:flutter_bloc_demo/rest_client/api_key.dart';
 import 'package:weather/weather.dart';
 
 void main() {
@@ -14,16 +15,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => WeatherBloc(
-        RepoProvider(
-          wf: WeatherFactory('2cc3a3443cb42e94fe1f06b7131e8c7a'),
+    return RepositoryProvider(
+      create: (_) => RepoProvider(wf: WeatherFactory(apiKey)),
+      child: BlocProvider(
+        create: (_) => WeatherBloc(RepoProvider(wf: WeatherFactory(apiKey))),
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData.dark(),
+          home: const HomeScreen(),
         ),
-      ),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData.dark(),
-        home: const HomeScreen(),
       ),
     );
   }
---
+++ lib/rest_client/api_key.dart
@@ -0,0 +1,1 @@
+const String apiKey ="2cc3a3443cb42e94fe1f06b7131e8c7a";
